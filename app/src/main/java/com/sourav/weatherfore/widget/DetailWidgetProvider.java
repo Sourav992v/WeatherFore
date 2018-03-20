9adb6331517627ff69da23f87119fb30bdd8edbb
Sourav Kumar Behera <sourav992v@gmail.com>
2018-03-20 13:00:37 +0530

    Add Detail Widget Provider

--- app/src/main/java/com/sourav/weatherfore/widget/DetailWidgetProvider.java
+++ app/src/main/java/com/sourav/weatherfore/widget/DetailWidgetProvider.java
@@ -34,7 +34,7 @@ public class DetailWidgetProvider extends AppWidgetProvider {
 
             // Create an Intent to launch MainActivity
             Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,0, intent, 0);
             views.setOnClickPendingIntent(R.id.widget, pendingIntent);
 
             // Set up the collection
